4848e25f92f0a3e93e3286cf2987ccbf866bc858
takyssj <alexammapu13@gmail.com>
2023-10-23 00:40:50 -0300

    'a'

--- Quiz/forms.py
+++ Quiz/forms.py
@@ -4,9 +4,9 @@ from .models import Pregunta, ElegirRespuesta, PreguntasRespondidas
 
 from django.contrib.auth.forms import UserCreationForm
 
-from django.contrib.auth import get_user_models
+from django.contrib.auth import get_user_model
 
-User = get_user_models()
+User = get_user_model()
 
 class ElegirInlineFormset(forms.BaseInlineFormSet):
     def clean(self):
